05f1ef2bf1bfa53f874802b3aad5e251d505ba6d
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-07-17 22:03:09 +0100

    Top Sellers Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner';
+import TopSellers from './TopSellers';
 
 export const Home = () => {
   return (
     <>
     <Banner />
+    <TopSellers/>
     </>
   )
 }
