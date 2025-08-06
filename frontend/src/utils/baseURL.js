47efa657f2f721eaf091858f2e8050f37484fd2c
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-08-06 12:08:28 +0100

    Fix CORS and deployment issues

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return import.meta.env.VITE_API_URL || "https://bookstore-system-3-bmv4.onrender.com/api";
+    const baseUrl = import.meta.env.VITE_API_URL || "https://bookstore-system-3-bmv4.onrender.com";
+    return `${baseUrl}/api`;
 }
 
 export default getBaseUrl;
