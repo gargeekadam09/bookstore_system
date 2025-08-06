4112137e831bc8a24ad691fc7bbb402816941910
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-08-06 13:07:01 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 // Import routes
 const bookRoutes = require('./src/books/book.route');
 const adminRoutes = require('./src/admin/admin.route');
